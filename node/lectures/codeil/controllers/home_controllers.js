5fb0e83195e2fbf16061d34debee6ea9656a5efd
yashwantgosavi <yashwantgosavi0@gmail.com>
2023-10-04 23:40:09 +0530

    manual Authentication - sign in/ sign up form

--- node/lectures/codeil/controllers/home_controllers.js
+++ node/lectures/codeil/controllers/home_controllers.js
@@ -3,3 +3,9 @@ module.exports.home=function(req, res){
         title: 'home'
     });
 }
+
+// module.exports.signUp=function(req, res){
+//     return res.render('home', {
+//         title: 'sign-Up'
+//     });
+// }
--- node/lectures/codeil/controllers/users_controllers.js
+++ node/lectures/codeil/controllers/users_controllers.js
@@ -1,12 +1,26 @@
 module.exports.profile = function (req, res) {
- return res.render('userProfile', {
-    title: 'checking',
-    nav:'layout',
- });
+  return res.render("userProfile", {
+    title: "checking",
+    nav: "layout",
+  });
 };
 
-module.exports.about = function(req, res){
-    return res.render('layout',{
-        title:'about'
-    });
+module.exports.about = function (req, res) {
+  return res.render("layout", {
+    title: "about",
+  });
+};
+
+  // render the sign up page
+module.exports.signUp = function (req, res) {
+  res.render("user_sign_up", {
+    title: "Codeil | Sign Up",
+  });
+};
+
+// render the sign in page
+module.exports.signIn = function (req, res) {
+  res.render("user_sign_in", {
+    title: "Codeil | Sign In",
+  });
 };
