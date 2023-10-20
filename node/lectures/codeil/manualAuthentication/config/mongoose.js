cde665acaaa7a754e4884e302387a203ceca8727
yashwant gosavi <yashwantgosavi0@gmail.com>
2023-10-20 19:41:10 +0530

    database setup

--- node/lectures/codeil/manualAuthentication/config/mongoose.js
+++ node/lectures/codeil/manualAuthentication/config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/codeial_development');
+mongoose.connect('mongodb+srv://yashwantgosavi0:8nbyHae088vDLEkh@cluster0.dgbhgot.mongodb.net/?retryWrites=true&w=majority');
 
 const db = mongoose.connection;
 
